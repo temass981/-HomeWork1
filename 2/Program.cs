af4b6af2d0a36cfcacaf5bbf1b4d5e5bb8215a92
Artyom <simstemasss@mail.ru>
2022-12-01 23:16:55 +0300

    Second task

---
+++ 2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b =int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int c =int.Parse(Console.ReadLine());
+int max =0;
+if (max<a){max=a;}
+if (max<b){max=b;}
+if (max<c){max=c;}
+Console.WriteLine("Самое большое число "+ max);
