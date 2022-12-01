17540897348faecea0a757713219e61abb390b18
Artyom <simstemasss@mail.ru>
2022-12-01 23:22:36 +0300

    Three task

---
+++ 3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a%2==0){
+    Console.WriteLine("Число "+ a + " четное");
+}else{Console.WriteLine("Число "+ a + " нечетное");}
