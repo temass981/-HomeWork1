b6d39baf1ae55757f7a6feb99c9c4df413b8b37a
Artyom <simstemasss@mail.ru>
2022-12-01 23:00:11 +0300

    firs task

---
+++ 1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if (a>b){
+    Console.WriteLine("Число " + a + " больше числа " + b);
+}else{Console.WriteLine("Число " + a + " меньше числа " + b);}
